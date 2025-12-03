7c70a29931d7ef290582ce71d86527a0d10f9276
Wenzi050802 <susannalinderman@gmail.com>
2025-12-02 18:58:41 -0800

    Update 11_Knockoff.R

--- 11_Knockoff.R
+++ 11_Knockoff.R
@@ -4,24 +4,24 @@
 
 run_knockoff_plus <- function(X, y, q = 0.1, model = "gaussian") {
   
-  knock_X <- knockoff::create.gaussian(X, mu = rep(0, ncol(X)), Sigma = cov(X))
+  # Estimate covariance & mean for Model-X Gaussian knockoff
+  mu_hat <- rep(0, ncol(X))
+  Sigma_hat <- cov(X)
   
-  if (model == "gaussian") {
-    fit <- glmnet::glmnet(cbind(X, knock_X), y, family = "gaussian")
-  } else {
-    fit <- glmnet::glmnet(cbind(X, knock_X), y, family = "binomial")
-  }
+  # Create Model-X Gaussian knockoffs (works even when p > n)
+  knock_X <- knockoff::create.gaussian(X, mu = mu_hat, Sigma = Sigma_hat)
   
+  # Fit Lasso for importance stats
+  fit <- glmnet::glmnet(cbind(X, knock_X), y, family = "gaussian")
   beta_hat <- coef(fit, s = fit$lambda.min)[-1]
   
   p <- ncol(X)
-  Z <- abs(beta_hat[1:p])
+  Z  <- abs(beta_hat[1:p])
   Zk <- abs(beta_hat[(p+1):(2*p)])
   
   W <- pmax(Z, Zk) * sign(Z - Zk)
   
   T <- knockoff::knockoff.threshold(W, fdr = q, offset = 1)
-  
   selected <- which(W >= T)
   
   list(selected = selected, W = W)
